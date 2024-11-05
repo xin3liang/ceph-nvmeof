4e79844f07c5cfbd149e4a4fab873aa4519dfd86
Gil Bregman <gbregman@il.ibm.com>
2024-11-05 15:21:21 +0200

    Allow any address for listeners using address 0.0.0.0 (or :: for IPv6). Fixes #935
    Signed-off-by: Gil Bregman <gbregman@il.ibm.com>

--- mk/demo.mk
+++ mk/demo.mk
@@ -1,12 +1,14 @@
 ## Demo:
 
 HOSTNQN=`cat /etc/nvme/hostnqn`
+NVMEOF_IO_PORT2=`expr $(NVMEOF_IO_PORT) + 1`
 # demo
 demo:
 	$(NVMEOF_CLI) subsystem add --subsystem $(NQN) --no-group-append
 	$(NVMEOF_CLI) namespace add --subsystem $(NQN) --rbd-pool $(RBD_POOL) --rbd-image $(RBD_IMAGE_NAME) --size $(RBD_IMAGE_SIZE) --rbd-create-image
 	$(NVMEOF_CLI) namespace add --subsystem $(NQN) --rbd-pool $(RBD_POOL) --rbd-image $(RBD_IMAGE_NAME)2 --size $(RBD_IMAGE_SIZE) --rbd-create-image --no-auto-visible
 	$(NVMEOF_CLI) listener add --subsystem $(NQN) --host-name `docker ps -q -f name=$(NVMEOF_CONTAINER_NAME)` --traddr $(NVMEOF_IP_ADDRESS) --trsvcid $(NVMEOF_IO_PORT)
+	$(NVMEOF_CLI) listener add --subsystem $(NQN) --host-name `docker ps -q -f name=$(NVMEOF_CONTAINER_NAME)` --traddr 0.0.0.0 --trsvcid $(NVMEOF_IO_PORT2)
 	$(NVMEOF_CLI_IPV6) listener add --subsystem $(NQN) --host-name `docker ps -q -f name=$(NVMEOF_CONTAINER_NAME)` --traddr $(NVMEOF_IPV6_ADDRESS) --trsvcid $(NVMEOF_IO_PORT) --adrfam IPV6
 	$(NVMEOF_CLI) host add --subsystem $(NQN) --host-nqn "*"
 	$(NVMEOF_CLI) namespace add_host --subsystem $(NQN) --nsid 2 --host-nqn $(HOSTNQN)
